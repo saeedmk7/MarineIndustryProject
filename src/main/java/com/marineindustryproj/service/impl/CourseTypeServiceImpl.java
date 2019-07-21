77fc40c6437690f07d623b023fe03a7a936c0fb8
Saeed <Saeed7394@gmail.com>
2019-07-21 23:17:39 +0430

    homepagereport

--- src/main/java/com/marineindustryproj/service/impl/CourseTypeServiceImpl.java
+++ src/main/java/com/marineindustryproj/service/impl/CourseTypeServiceImpl.java
@@ -8,12 +8,14 @@ import com.marineindustryproj.service.mapper.CourseTypeMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -30,9 +32,12 @@ public class CourseTypeServiceImpl implements CourseTypeService {
 
     private final CourseTypeMapper courseTypeMapper;
 
-    public CourseTypeServiceImpl(CourseTypeRepository courseTypeRepository, CourseTypeMapper courseTypeMapper) {
+    private final CacheManager cacheManager;
+
+    public CourseTypeServiceImpl(CourseTypeRepository courseTypeRepository, CourseTypeMapper courseTypeMapper, CacheManager cacheManager) {
         this.courseTypeRepository = courseTypeRepository;
         this.courseTypeMapper = courseTypeMapper;
+        this.cacheManager = cacheManager;
     }
 
     /**
@@ -47,6 +52,7 @@ public class CourseTypeServiceImpl implements CourseTypeService {
 
         CourseType courseType = courseTypeMapper.toEntity(courseTypeDTO);
         courseType = courseTypeRepository.save(courseType);
+        clearJobCaches();
         return courseTypeMapper.toDto(courseType);
     }
 
@@ -96,5 +102,9 @@ public class CourseTypeServiceImpl implements CourseTypeService {
     public void delete(Long id) {
         log.debug("Request to delete CourseType : {}", id);
         courseTypeRepository.deleteById(id);
+        clearJobCaches();
+    }
+    private void clearJobCaches() {
+        Objects.requireNonNull(cacheManager.getCache(courseTypeRepository.ALL_COURSETYPE_CACHE)).clear();
     }
 }
